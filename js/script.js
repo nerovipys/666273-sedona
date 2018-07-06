8ab45ae92795563da4bf02e5df21faf0b8f48296
nerovipys <nerovipys2@gmail.com>
2018-07-06 18:55:26 +0300

    final of final of final

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ var form = document.querySelector('.search-form');
 var arrival = document.getElementById('date-check');
 var departure = document.getElementById('date-out');
 
-form.classList.remove("search-form-active");
+form.classList.remove('search-form-active');
 
-btn.addEventListener("click", function (evt) {
-  if (form.classList.contains("search-form-active")) {
-    form.classList.remove("search-form-active");
-    form.classList.remove("modal-error");
+btn.addEventListener('click', function (evt) {
+  if (form.classList.contains('search-form-active')) {
+    form.classList.remove('search-form-active');
+    form.classList.remove('modal-error');
   }
   else {
-    form.classList.add("search-form-active");
+    form.classList.add('search-form-active');
     arrival.focus();
   }
 });
 
-form.addEventListener("submit", function (evt) {
+form.addEventListener('submit', function (evt) {
   if (!arrival.value || !departure.value) {
     evt.preventDefault();
-    form.classList.remove("modal-error");
+    form.classList.remove('modal-error');
     form.offsetWidth = form.offsetWidth;
-    form.classList.add("modal-error");
+    form.classList.add('modal-error');
   }
 });
 
